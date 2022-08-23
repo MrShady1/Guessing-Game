c8e47ef315406846cd1adc0dfbe0296be0abcfbc
MAC <user@MACs-MacBook-Pro.local>
2022-08-23 20:11:15 +0100

    Hangman

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 import random
 
 from words import Words
+from hangman_visual import lives_visual_dict
 import string
 
 
@@ -9,7 +10,7 @@ def get_valid_word(Words):
     while '-' in word or '' in word:
         word = random.choice(Words)
 
-    return word
+    return word.upper()
 
 def hangman():
     word = get_valid_word(Words)
@@ -17,7 +18,7 @@ def hangman():
     alphabet = set(string.ascii_uppercase) 
     used_letters = set() # to keep track of what users have guessed
 
-    lives = 6
+    lives = 7
 
     #getting user input
     while len(word_letters) > 0 and lives > 0:
@@ -27,24 +28,28 @@ def hangman():
 
         #what current word is (ie W _ R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
+        print(lives_visual_dict[lives])
         print('Current word: ', ''.join(word_list))
         user_letter = input('Guess a letter:  ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+                print('')
             else:
                 lives = lives -1
-                print('Letter not in the word;')
+                print('\nLetter', user_letter, 'is not in the word')
 
         elif user_letter in word_letters:
                 print(f'You are have already used that character. Please try again')
         else:
-            print('invalid character, Try again')  
+            print('\ninvalid character, Try again')  
 
     if lives == 0:
+        print(lives_visual_dict[lives])
         print('You Died, Sorry')
     else:
         print('You Guessed the word', word, '!!')                
               
-print(hangman)
+if __name__ == '__main__':
+    hangman()
